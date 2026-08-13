1858849055c3a1f8a5765481dac1565c30f758e0
jazminyoshida <jazminyoshida.24@gmail.com>
2026-08-13 15:40:04 +0000

    hihih

--- story.py
+++ story.py
@@ -14,15 +14,19 @@ def main():
     # print("Hello", end=" ")
     # print(planet)
 
-    name = input("What is your name? ")
-    color = input("Tell me a color: ")
-    adj = input("Tell me an adjetive: ")
-    goal = input("A goal you want tp achieve: ")
+    name = input("What is your name? ").strip().title()
+    color = input("Tell me a color: ").strip().lower()
+    adj = input("Tell me an adjetive: ").strip().lower()
+    goal = input("A goal you want tp achieve: ").strip().lower()
 
-    print(f"Hello, {name}!", end="\n\n")
+    print(f"Hello, {name}!")
+    print()
 
     print("This is your story:")
-    print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}.")
+    print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}. ")
+
+    print("This is your story:")
+    print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}. ".upper())
 
 if __name__=="__main__":
     main()
