bcba9cb123a38a17a7528293f90bed1aa8ae6bd6
jazminyoshida <jazminyoshida.24@gmail.com>
2026-08-19 18:01:53 +0000

    something

---
+++ update-variable.py
@@ -0,0 +1,39 @@
+def main():
+    # Augmentd Assignment Operator
+    # x += y
+    score= 10
+    score += 10
+    print(score)
+
+    #Substraction Assignment Operartor
+    # x -= y
+    grade = 100
+    grade -= 15
+
+    print(grade)
+
+    # Muliplication Assignment Operartor
+    # x *= y
+    balance = 10
+    balance *= 4
+
+    print(balance)
+
+    # Division Assignment Operator
+    # x /= y
+    number = 10
+    number /= 5
+
+    print(number)
+
+    # Modulus Assignment Operator
+    # x %= y
+
+    digit = 10
+    digit %= 4
+
+    print(digit)
+
+if __name__ == "__main__":
+    main()
+
