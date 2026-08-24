da1a62b53045c46ecdebe051490835c64ca236cb
jazminyoshida <jazminyoshida.24@gmail.com>
2026-08-24 14:59:24 +0000

    password

---
+++ password.py
@@ -0,0 +1,12 @@
+def main():
+    password = 478890
+    user = int(input("What is the password? "))
+    if user == password:
+                print("correct answer")
+
+    print("see you later")
+
+
+
+if __name__=="__main__":
+   main()
