0160a96b7e3d773db6e6d1dcbc64324ad25b06b5
jazminyoshida <jazminyoshida.24@gmail.com>
2026-09-01 21:42:20 +0000

    SDFG

---
+++ coinflip2.py
@@ -0,0 +1,20 @@
+import random
+def main():
+   coin = ["heads","tails"]
+   attempts = 3
+   while attempts > 0:
+      flip = random.choice(coin)
+      guess = input("Heads or Tails?: ").strip().lower()
+
+      print("The coin landed on",flip)
+
+      if guess == flip:
+         print("Winner")
+         break
+      else:
+         print("Loser")
+         attempts -= 1
+         print("Attempts left:", attempts)
+
+if __name__=="__main__":
+   main()
