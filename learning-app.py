032ff22a72e7b026679b91bc37d3704ad57a6865
jazminyoshida <jazminyoshida.24@gmail.com>
2026-09-04 15:48:22 +0000

    ghjk

---
+++ learning-app.py
@@ -0,0 +1,16 @@
+import random
+def main():
+   #app name
+   print("StudyUp")
+   number1 = random.randint(10,99)
+   number2 = random.randint(10,99)
+
+   
+
+   print(f"Try to do this:{number1} + {number2}")
+
+
+
+
+if __name__=="__main__":
+   main()
