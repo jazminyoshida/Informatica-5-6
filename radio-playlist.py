95f77292331877a8e2164e21802642101e201b14
jazminyoshida <jazminyoshida.24@gmail.com>
2026-09-15 15:49:55 +0000

    jdykju

---
+++ radio-playlist.py
@@ -0,0 +1,31 @@
+import time
+def main():
+   playlist = ["Boston", "Dracula", "I knew it, I knew you", "Hate that I made you love me", "Risk it all"]
+   playlist.append("Be by you")
+
+   playlist.insert(0,"Bohemian Rhapsody")
+
+   playlist.pop(4)
+
+   print(playlist)
+
+   print(playlist.index("Risk it all"))
+   print("Number of songs in playlist:", len(playlist))
+   playlist.reverse()
+   print(playlist)
+   playlist.sort()
+   print(playlist)
+
+   #challenge
+   repeat = len(playlist)
+   while repeat > 0:
+      print(playlist)
+      song = playlist[0]
+      playlist.pop(0)
+      playlist.append(song)
+      repeat -= 1
+      time.sleep(3)
+
+
+if __name__=="__main__":
+   main()
