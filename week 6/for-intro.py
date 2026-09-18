f183a43f85a04e9fcfdc06f74afdc4c284c89864
jazminyoshida <jazminyoshida.24@gmail.com>
2026-09-18 21:25:06 +0000

    hero

--- week 6/for-intro.py
+++ week 6/for-intro.py
@@ -1,8 +1,8 @@
 def main():
    doctrine = ["Fe en el Señor Jesucristo", "Arrepentimiento", "Bautizmo", "Recivir el don del espiritu santo", "Preseverar"]
 
-   for hero in doctrine:
-      print(hero)
+   for hero in range(len(doctrine)):
+      print(f"{hero+1} {doctrine[hero]}")
 
 
 if __name__=="__main__":
