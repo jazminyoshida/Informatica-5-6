302a7ee62605896b34f1cfe2fba2dcbfc63cda9d
jazminyoshida <jazminyoshida.24@gmail.com>
2026-09-18 20:19:51 +0000

    vbn

---
+++ week 6/for-intro.py
@@ -0,0 +1,9 @@
+def main():
+   doctrine = ["Fe en el Señor Jesucristo", "Arrepentimiento", "Bautizmo", "Recivir el don del espiritu santo", "Preseverar"]
+
+   for hero in doctrine:
+      print(hero)
+
+
+if __name__=="__main__":
+   main()
